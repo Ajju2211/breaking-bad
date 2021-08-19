a04a30bb5a6a3a8b513b8f01a5db9a02e870c64c
Mohammad Azharuddin <43109538+Ajju2211@users.noreply.github.com>
2021-08-19 15:45:44 +0530

    Update SearchBar.js

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{ useEffect } from "react";
 import { useHistory } from 'react-router-dom'
 import { FaSearch } from 'react-icons/fa'
 import { useQuery } from "../utils";
@@ -7,6 +7,32 @@ import "./styles.css";
 export default function SearchBar({ onSubmit, onChange, text, ...props }) {
   let query = useQuery();
   const history = useHistory();
+  const searchNow = () => {
+              let trimmedText = text.trim();
+              trimmedText = trimmedText.replace(/\s/g, ' ');
+              if (trimmedText.length < 1) {
+                alert('Enter min characters');
+                return;
+              }
+              trimmedText = trimmedText.replace(/\s/g, '+');
+              query.set('name', trimmedText);
+              history.push({ search: query.toString() })
+              if (onSubmit) {
+                onSubmit(query.toString());
+              }
+  }
+  useEffect(() => {
+     const enterListener = event => {
+      if (event.code === "Enter" || event.code === "NumpadEnter") {
+        event.preventDefault();
+        searchNow();
+      }
+    };
+    window.document.querySelector('input[aria-describedby="search-input"]').addEventListener("keydown", enterListener);
+    return () => {
+      window.document.querySelector('input[aria-describedby="search-input"]').removeEventListener("keydown", enterListener);
+    };
+  })
   return (
     <div className={props.className + " a"}>
       <div class="input-group mb-3">
@@ -21,18 +47,7 @@ export default function SearchBar({ onSubmit, onChange, text, ...props }) {
         <div class="input-group-append">
           <span class="input-group-text" id="search-input">
             <FaSearch onClick={() => {
-              let trimmedText = text.trim();
-              trimmedText = trimmedText.replace(/\s/g, ' ');
-              if (trimmedText.length < 1) {
-                alert('Enter min characters');
-                return;
-              }
-              trimmedText = trimmedText.replace(/\s/g, '+');
-              query.set('name', trimmedText);
-              history.push({ search: query.toString() })
-              if (onSubmit) {
-                onSubmit(query.toString());
-              }
+              searchNow();
             }} />
           </span>
         </div>
